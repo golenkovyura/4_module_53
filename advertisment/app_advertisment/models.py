01bc2c793885ba440d08b6dd1cce6a3317f85101
Юрий Голенков <yur.golenkoff@yandex.ru>
2023-08-16 16:53:16 +0300

    Код после 10 урока

--- advertisment/app_advertisment/models.py
+++ advertisment/app_advertisment/models.py
@@ -16,7 +16,7 @@ class Advertisement(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
-    image = models.ImageField('Изображение', upload_to='advertisements/')
+    image = models.ImageField('Изображение', upload_to='advertisements/', null=True, blank=True)
 
     @admin.display(description='Дата создания')
     def created_date(self):
@@ -35,6 +35,13 @@ class Advertisement(models.Model):
                 '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
             )
         return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description='Фото')
+    def get_html_image(self):
+        if self.image:
+            return format_html(
+                '<img src="{url}" style="max-width: 80px; max-height: 80px;"', url=self.image.url
+            )
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
